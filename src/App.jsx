a9899ac755a84be29d7664ee0949ae7d3bb79b05
Jirka-Mayer <jirkamayer97@gmail.com>
2023-01-05 10:43:55 +0100

    Added custom Quill attributors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import * as styles from "./App.module.scss"
 import { Report } from "./Report"
+import { useState } from "react"
 
 export function App() {
+  const [count, setCount] = useState(0)
+  
   return (
     <>
-      <p>Hello world!</p>
+      <p>Hello world {count}x!</p>
       <button
         className={styles["my-button"]}
-        onClick={() => alert("Hello!")}
+        onClick={() => setCount(count + 1)}
       >Click me!</button>
       <Report />
     </>
--- src/Report.jsx
+++ src/Report.jsx
@@ -1,10 +1,38 @@
 import React, { useState, useEffect, useRef } from "react"
 import Quill from "quill"
 import * as styles from "./Report.module.scss"
+import { highlightAttributorFactory } from "./highlightAttributorFactory"
+
+Quill.register(
+  highlightAttributorFactory(
+    "highlight",
+    styles["highlight"],
+    Object.keys(styles)
+      .filter(k => k.indexOf("highlight-") === 0)
+      .map(k => k.substring(10))
+  )
+)
 
 export function Report() {
   const quillRef = useRef(null)
   let quill = null
+
+  function focusField(field) {
+    if (styles["focus-" + field] === undefined) {
+      // TODO: implement some centralized error reporting
+      alert(`ERROR: The field ${field} is not in the stylesheet. Update the @for loop bounds accordingly.`)
+      return
+    }
+
+    const matches = (quillRef.current.getAttribute('class') || '')
+      .split(/\s+/)
+      .filter((name) => name.indexOf(styles["focus"] + "-") === 0)
+    matches.forEach((name) => {
+      quillRef.current.classList.remove(name)
+    })
+    
+    quillRef.current.classList.add(styles["focus"] + "-" + field)
+  }
 
   useEffect(() => {
     quill = new Quill(quillRef.current, {
@@ -13,19 +41,40 @@ export function Report() {
       formats: [
         // inline
         "bold", "italic", "underline", "strike", "script",
-
+        
         // block
-        "header"
+        "header",
+        
+        // custom
+        "highlight"
       ]
     })
+
+    quill.setContents([
+      { insert: "Hello " },
+      { insert: "world!", attributes: { highlight: "0" } },
+      { insert: "\n" },
+      { insert: "Hello", attributes: { highlight: "1" } },
+      { insert: " world!\n"}
+    ])
+
+    focusField("0")
+
+    // expose quill to the browser console
+    window.quill = quill
   })
 
   return (
     <div className={styles["debug-container"]}>
       <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet"></link>
       <div ref={quillRef}></div>
+      <hr />
       <button onClick={() => {quill.enable(false)}}>Disable editing</button>
       <button onClick={() => {quill.enable(true)}}>Enable editing</button>
+      <hr />
+      {[...Array(5).keys()].map((x, i) =>
+        <button key={i} onClick={() => {focusField(i.toString())}}>Focus field {i}</button>
+      )}
     </div>
   )
 }
