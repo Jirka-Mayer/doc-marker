68066cd56f9b3905d9487b3b653db4836e07199c
Jirka-Mayer <jirkamayer97@gmail.com>
2023-01-05 16:01:01 +0100

    Fixed highlight range overlaps

--- src/Report.jsx
+++ src/Report.jsx
@@ -1,19 +1,33 @@
 import React from "react"
 import Quill from "quill"
 import * as styles from "./Report.module.scss"
-import { highlightAttributorFactory } from "./highlightAttributorFactory"
 import { AppMode } from "./AppMode"
 
-Quill.register(
-  highlightAttributorFactory(
-    "highlight",
-    styles["highlight"],
-    Object.keys(styles) // whitelist of possible values
-      .filter(k => k.indexOf("highlight-") === 0)
-      .map(k => k.substring(10))
-  ),
-  true // no warnings
-)
+// set of all of our CSS classes
+const stylesClassSet = new Set(Object.values(styles))
+
+// field names defined in the style sheet
+const allowedFieldNames = Object.keys(styles)
+  .filter(k => k.indexOf("active-") === 0)
+  .map(k => k.substring("active-".length))
+  .sort()
+
+// register one attributor for each field name
+//
+// NOTE: The attributor needs to have some value,
+// so we'll use "yes" as the only allowed value.
+const Parchment = Quill.import("parchment")
+for (let fieldName in allowedFieldNames) {
+  let attributor = new Parchment.Attributor.Class(
+    "highlight-" + fieldName,
+    styles[`highlight-${fieldName}-yes`].slice(0, -4), // -4 for the "-yes" suffix
+    {
+      scope: Parchment.Scope.INLINE,
+      whitelist: ["yes"] // allow only "yes" as the value
+    }
+  )
+  Quill.register(attributor, true)
+}
 
 // NOTE: Class compomnents are better suited for integration with 3rd party
 // libraries that manipulate DOM than functional components.
@@ -25,6 +39,7 @@ export class Report extends React.Component {
 
     this.containerElement = null
     this.quillElement = null
+    this.selectionChangehandler = null
   }
   
   componentDidMount() {
@@ -42,22 +57,23 @@ export class Report extends React.Component {
         "header",
         
         // custom
-        "highlight"
+        ...allowedFieldNames.map(n => "highlight-" + n)
       ]
     })
 
     this.quill.setContents([
       { insert: "Hello " },
-      { insert: "world!", attributes: { highlight: "0" } },
+      { insert: "world!", attributes: { "highlight-0": "yes" } },
       { insert: "\n" },
-      { insert: "Hello", attributes: { highlight: "1" } },
+      { insert: "Hello", attributes: { "highlight-1": "yes" } },
       { insert: " world!\n"}
     ])
 
     this.renderAppMode()
     this.renderFieldActivity()
 
-    this.quill.on("selection-change", this.onSelectionChange)
+    this.selectionChangehandler = this.onSelectionChange.bind(this)
+    this.quill.on("selection-change", this.selectionChangehandler)
 
     // expose quill to the browser console
     window.quill = this.quill
@@ -72,7 +88,7 @@ export class Report extends React.Component {
   }
 
   componentWillUnmount() {
-    this.quill.off("selection-change", this.onSelectionChange)
+    this.quill.off("selection-change", this.selectionChangehandler)
     
     this.quill = null
     this.quillElement.remove()
@@ -80,7 +96,29 @@ export class Report extends React.Component {
   }
 
   onSelectionChange(range, oldRange, source) {
-    // TODO: when the mode is "annotations", handle clicks and drags
+    if (this.props.mode === AppMode.ANNOTATE_HIGHLIGHTS && range) {
+      if (range.length === 0)
+        this.handleAnnotationClick(range.index)
+      else
+        this.handleAnnotationDrag(range.index, range.length)
+    }
+  }
+
+  handleAnnotationClick(index) {
+    //
+  }
+
+  handleAnnotationDrag(index, length) {
+    if (this.props.activeFieldName === null)
+      return
+    
+    // TODO: round selection to words
+    
+    let formats = {}
+    formats["highlight-" + this.props.activeFieldName] = "yes"
+    this.quill.formatText(index, length, formats)
+
+    this.quill.setSelection(index + length, 0)
   }
 
   renderAppMode() {
@@ -96,14 +134,10 @@ export class Report extends React.Component {
   renderFieldActivity() {
     const activeFieldName = this.props.activeFieldName
 
-    // remove all activity css classes
-    const classNames = this.quillElement.getAttribute('class') || ''
-    classNames
-      .split(/\s+/)
-      .filter((name) => name.indexOf(styles["active"] + "-") === 0)
-      .forEach((name) => {
-        this.quillElement.classList.remove(name)
-      })
+    // remove all of our css classes
+    Array.from(this.quillElement.classList.values())
+      .filter(name => stylesClassSet.has(name))
+      .forEach(name => this.quillElement.classList.remove(name))
 
     // no field is active
     if (activeFieldName === null)
@@ -116,7 +150,7 @@ export class Report extends React.Component {
     }
     
     // add the proper activity css class
-    this.quillElement.classList.add(styles["active"] + "-" + activeFieldName)
+    this.quillElement.classList.add(styles["active-" + activeFieldName])
   }
 
   render() {
@@ -126,6 +160,7 @@ export class Report extends React.Component {
 
         <pre>Mode: {this.props.mode}</pre>
         <pre>Active field name: {JSON.stringify(this.props.activeFieldName)}</pre>
+        <pre>Allowed field names: {JSON.stringify(allowedFieldNames)}</pre>
   
         <div ref={el => this.containerElement = el}></div>
       </div>
