e4564316cbb756b230ad313be72ebf7ba707f411
Jirka-Mayer <jirkamayer97@gmail.com>
2023-01-05 11:16:51 +0100

    Cleaned up the quill react component

--- src/Report.jsx
+++ src/Report.jsx
@@ -7,35 +7,33 @@ Quill.register(
   highlightAttributorFactory(
     "highlight",
     styles["highlight"],
-    Object.keys(styles)
+    Object.keys(styles) // whitelist of possible values
       .filter(k => k.indexOf("highlight-") === 0)
       .map(k => k.substring(10))
-  )
+  ),
+  true // no warnings
 )
 
-export function Report() {
-  const quillRef = useRef(null)
-  let quill = null
+// NOTE: Class compomnents are better suited for integration with 3rd party
+// libraries that manipulate DOM than functional components.
+export class Report extends React.Component {
+  constructor(props) {
+    super(props)
 
-  function focusField(field) {
-    if (styles["focus-" + field] === undefined) {
-      // TODO: implement some centralized error reporting
-      alert(`ERROR: The field ${field} is not in the stylesheet. Update the @for loop bounds accordingly.`)
-      return
+    this.state = {
+      // TODO: this will later come to us via props
+      focusedField: "0"
     }
 
-    const matches = (quillRef.current.getAttribute('class') || '')
-      .split(/\s+/)
-      .filter((name) => name.indexOf(styles["focus"] + "-") === 0)
-    matches.forEach((name) => {
-      quillRef.current.classList.remove(name)
-    })
-    
-    quillRef.current.classList.add(styles["focus"] + "-" + field)
+    this.containerElement = null
+    this.quillElement = null
   }
+  
+  componentDidMount() {
+    this.quillElement = document.createElement("div")
+    this.containerElement.appendChild(this.quillElement)
 
-  useEffect(() => {
-    quill = new Quill(quillRef.current, {
+    this.quill = new Quill(this.quillElement, {
       theme: "snow",
       placeholder: "Paste discharge report here...\nTODO: Localization",
       formats: [
@@ -50,7 +48,7 @@ export function Report() {
       ]
     })
 
-    quill.setContents([
+    this.quill.setContents([
       { insert: "Hello " },
       { insert: "world!", attributes: { highlight: "0" } },
       { insert: "\n" },
@@ -58,23 +56,60 @@ export function Report() {
       { insert: " world!\n"}
     ])
 
-    focusField("0")
+    this.focusField(this.state.focusedField)
 
     // expose quill to the browser console
-    window.quill = quill
-  })
+    window.quill = this.quill
+  }
 
-  return (
-    <div className={styles["debug-container"]}>
-      <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet"></link>
-      <div ref={quillRef}></div>
-      <hr />
-      <button onClick={() => {quill.enable(false)}}>Disable editing</button>
-      <button onClick={() => {quill.enable(true)}}>Enable editing</button>
-      <hr />
-      {[...Array(5).keys()].map((x, i) =>
-        <button key={i} onClick={() => {focusField(i.toString())}}>Focus field {i}</button>
-      )}
-    </div>
-  )
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.focusedField !== this.state.focusedField) {
+      this.focusField(this.state.focusedField)
+    }
+  }
+
+  componentWillUnmount() {
+    this.quill = null
+    this.quillElement.remove()
+    this.quillElement = null
+  }
+
+  focusField(field) {
+    if (styles["focus-" + field] === undefined) {
+      // TODO: implement some centralized error reporting
+      alert(`ERROR: The field ${field} is not in the stylesheet. Update the @for loop bounds accordingly.`)
+      return
+    }
+
+    const matches = (this.quillElement.getAttribute('class') || '')
+      .split(/\s+/)
+      .filter((name) => name.indexOf(styles["focus"] + "-") === 0)
+    matches.forEach((name) => {
+      this.quillElement.classList.remove(name)
+    })
+    
+    this.quillElement.classList.add(styles["focus"] + "-" + field)
+  }
+
+  render() {
+    return (
+      <div className={styles["debug-container"]}>
+        <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet"></link>
+  
+        <div ref={el => this.containerElement = el}></div>
+  
+        <hr />
+        <button onClick={() => {quill.enable(false)}}>Disable editing</button>
+        <button onClick={() => {quill.enable(true)}}>Enable editing</button>
+        <hr />
+        {[...Array(5).keys()].map((x, i) =>
+          <button
+            key={i}
+            disabled={this.state.focusedField == i.toString()}
+            onClick={() => {this.setState({ focusedField: i.toString() })}}
+          >Focus field {i}</button>
+        )}
+      </div>
+    )
+  }
 }
