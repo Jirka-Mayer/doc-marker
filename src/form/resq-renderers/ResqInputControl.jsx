9e3bf61bbda81980f70bd3160c0882433c657dbc
Jirka-Mayer <jirkamayer97@gmail.com>
2023-01-16 13:28:48 +0100

    Sketched out form field style

--- src/form/resq-renderers/ResqInputControl.jsx
+++ src/form/resq-renderers/ResqInputControl.jsx
@@ -1,4 +1,4 @@
-import { InputLabel, FormControl, IconButton, OutlinedInput, Input, Paper } from "@mui/material"
+import { InputLabel, FormControl, IconButton, OutlinedInput, Input, Paper, FormHelperText } from "@mui/material"
 import InputBase from "@mui/material/InputBase"
 import Divider from "@mui/material/Divider"
 import DoneIcon from "@mui/icons-material/Done"
@@ -7,6 +7,8 @@ import ManageSearchIcon from '@mui/icons-material/ManageSearch'
 import BorderColorIcon from "@mui/icons-material/BorderColor"
 import { useContext } from "react"
 import { FormContext } from "../FormContext"
+
+import * as styles from "./renderers.module.scss"
 
 export function ResqInputControl(props) {
   const formContext = useContext(FormContext)
@@ -42,29 +44,39 @@ export function ResqInputControl(props) {
   
   return (
     <Paper
-      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center' }}
+      className={styles["paper"]}
     >
-      <IconButton
-        disabled={false}
-        color={isFieldActive ? "primary" : "default"}
-        sx={{ p: '10px' }}
-      >
-        {/* <ShortTextIcon /> */}
-        <ManageSearchIcon />
-        {/* <BorderColorIcon fontSize="small"/> */}
-      </IconButton>
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder={`${path} (${id})`}
-        value={data}
-        onFocus={onFocus}
-        onChange={onChange}
-      />
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton color="default" disabled={false} sx={{ p: '10px' }}>
-        <DoneIcon />
-      </IconButton>
-      <p>{errors}</p>
+      <InputLabel
+        className={styles["label"]}
+        htmlFor={id + "-input"}
+      >{ label }</InputLabel>
+      <div className={styles["row"]}>
+        <IconButton
+          disabled={false}
+          color={isFieldActive ? "primary" : "default"}
+          sx={{ p: '10px' }}
+        >
+          {/* <ShortTextIcon /> */}
+          <ManageSearchIcon />
+          {/* <BorderColorIcon fontSize="small"/> */}
+        </IconButton>
+        <InputBase
+          id={id + "-input"}
+          sx={{ ml: 1, flex: 1 }}
+          placeholder={`${path} (${id})`}
+          value={data}
+          onFocus={onFocus}
+          onChange={onChange}
+        />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <IconButton color="default" disabled={false} sx={{ p: '10px' }}>
+          <DoneIcon />
+        </IconButton>
+      </div>
+      <FormHelperText
+        className={styles["helper-text"]}
+        error={true}
+      >{errors}</FormHelperText>
     </Paper>
   )
 }
