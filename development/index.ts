35c8d8b63caff15730a3ec60bc76bc206c3aadbf
Jirka Mayer <jirkamayer97@gmail.com>
2025-09-10 12:49:34 +0200

    Fixed react-scan

--- development/index.ts
+++ development/index.ts
@@ -1,23 +1,35 @@
-import { bootstrapDocMarker } from "../src/index" // like "doc-marker"
-
-bootstrapDocMarker({
-  element: document.getElementById("doc-marker"),
-
-  customization: {
-    name: "DocMarker Demo Customization"
-  },
-
-  // the orange ÚFAL theme used for the demo
-  theme: {
-    palette: {
-      mode: "light",
-      primary: {
-        main: "#f47b20", // ÚFAL orange
-        contrastText: "rgba(255,255,255,0.95)" // white text over the primary
-      },
-      secondary: {
-        main: "#f47b20" // light yellow
-      }
-    }
+async function main() {
+  // Debugging tool that displays react re-renders of components.
+  // Must be imported before React and React DOM.
+  // https://github.com/aidenybai/react-scan
+  if (process.env.NODE_ENV === "development") {
+    await import("react-scan");
   }
-})
+
+  // deferred import so that react-scan is imported first
+  const { bootstrapDocMarker } = await require("../src/index");
+
+  bootstrapDocMarker({
+    element: document.getElementById("doc-marker"),
+
+    customization: {
+      name: "DocMarker Demo Customization",
+    },
+
+    // the orange ÚFAL theme used for the demo
+    theme: {
+      palette: {
+        mode: "light",
+        primary: {
+          main: "#f47b20", // ÚFAL orange
+          contrastText: "rgba(255,255,255,0.95)", // white text over the primary
+        },
+        secondary: {
+          main: "#f47b20", // light yellow
+        },
+      },
+    },
+  });
+}
+
+main();
